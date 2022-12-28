06d432b10209e52e6f0a5afe5bfd5b6f0642feee
lenarttreven <lenart.treven44@gmail.com>
2022-12-28 11:49:33 +0100

    renaming

--- main.cpp
+++ main.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
-#include "day_1/day_1.h"
-#include "day_2/day_2.h"
-#include "day_3/day_3.h"
-#include "day_4/day_4.h"
-#include "day_5/day_5.h"
-#include "day_6/day_6.h"
-#include "day_7/day_7.h"
-#include "day_8/day_8.h"
-#include "day_9/day_9.h"
+#include "day_01/day_1.h"
+#include "day_02/day_2.h"
+#include "day_03/day_3.h"
+#include "day_04/day_4.h"
+#include "day_05/day_5.h"
+#include "day_06/day_6.h"
+#include "day_07/day_7.h"
+#include "day_08/day_8.h"
+#include "day_09/day_9.h"
 #include "day_10/day_10.h"
 
 using namespace std;
